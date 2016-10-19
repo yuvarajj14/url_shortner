ff75a75b00d97651dbc7e4d067e00c6af42c0d60
yuvaraj <yuvaraj@fromahome.com>
2016-10-19 08:32:11 +0530

    test for hitting the shorted url

--- spec/features/links_spec.rb
+++ spec/features/links_spec.rb
@@ -6,5 +6,7 @@ describe 'managing advertisement by business development admin', js: true do
     click_button('Save Link')
     expect(page).to have_content('https://www.google.co.in/?gfe_rd=cr&ei=u-EFWOLiJeOK8Qf4kbmQCg&gws_rd=ssl')
     expect(page).to have_content(Link.last.shorted_url)
+    href = "http://localhost:3000/#{Link.last.shorted_url}"
+    expect(page).to have_content("google")
   end
 end
